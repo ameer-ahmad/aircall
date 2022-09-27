7147f786bdd3eed014990fab4646732d67acd90a
ameer-ahmad <ahmaamee@sheridancollege.ca>
2022-09-27 00:48:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+import ActivityFeed from "./components/ActivityFeed"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <ActivityFeed />
     </div>
   );
 }
